bfdfcc397c9f7eef85e4418057ed9e1f99095a1c
Eren Aydoslu <erenaydoslue@icloud.com>
2024-05-14 21:47:10 +0200

    I think it's finally fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const handleInputSubmit = async () => {
     try {
-      const response = await fetch('/submit_input', {
+      const response = await fetch('http://127.0.0.1:8080/submit_input', {  // Use the full URL here
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
