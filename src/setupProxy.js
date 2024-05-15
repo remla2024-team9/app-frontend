9f350bb47086480b20fa3bccf6d5f4f1c17c8044
Eren Aydoslu <erenaydoslue@icloud.com>
2024-05-15 19:16:11 +0200

    changed target url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/submit_input',
         createProxyMiddleware({
-            target: 'http://127.0.0.1:8080',
+            target: 'http://app-service:8080',
             changeOrigin: true,
         })
     );
